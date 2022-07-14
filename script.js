97dcdc8319cd5d202e839eb1896ed682d90e9929
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2022-07-14 16:01:47 +0530

    adding logo file

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // DOM elements
-    const button = document.querySelector("button");
-    const quote = document.querySelector("blockquote p");
-    const cite = document.querySelector("blockquote cite");
+document.addEventListener('DOMContentLoaded', () => {
+  // DOM elements
+  const button = document.querySelector('button')
+  const quote = document.querySelector('blockquote p')
+  const cite = document.querySelector('blockquote cite')
+  const tweetMe = document.getElementById('tweetMe')
 
-    async function updateQuote() {
-        // Fetch a random quote from the Quotable API
-        const response = await fetch("https://api.quotable.io/random");
-        const data = await response.json();
-        if (response.ok) {
-            // Update DOM elements
-            quote.textContent = data.content;
-            cite.textContent = data.author;
-        } else {
-            quote.textContent = "An error occured";
-            console.log(data);
-        }
+  async function updateQuote() {
+    // Fetch a random quote from the Quotable API
+    const response = await fetch('https://api.quotable.io/random')
+    const data = await response.json()
+    if (response.ok) {
+      // Update DOM elements
+      quote.textContent = data.content
+      cite.textContent = data.author
+    } else {
+      quote.textContent = 'An error occured'
+      console.log(data)
     }
+  }
 
-    // Attach an event listener to the `button`
-    button.addEventListener("click", updateQuote);
+  const tweetNow = () => {
+    let tweetPost = `https://twitter.com/intent/tweet?text=${quote.textContent} ${cite.textContent}`
+    window.open(tweetPost)
+  }
 
-    // call updateQuote once when page loads
-    updateQuote();
-});
+  document.getElementById('social_media').onclick = function () {
+    location.href = 'https://www.instagram.com/one_motivational_quote/'
+  }
+
+  // Attach an event listener to the `button`
+  button.addEventListener('click', updateQuote)
+  tweetMe.addEventListener('click', tweetNow)
+
+  // call updateQuote once when page loads
+  updateQuote()
+})
